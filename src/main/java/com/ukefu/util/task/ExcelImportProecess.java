e78d7a6c5ac3c77d840c78c2dde805384e500456
优客服-多渠道客服系统 <ricy_fu@126.com>
2017-07-05 21:19:06 +0800

    no commit message

--- src/main/java/com/ukefu/util/task/ExcelImportProecess.java
+++ src/main/java/com/ukefu/util/task/ExcelImportProecess.java
@@ -107,7 +107,7 @@ public class ExcelImportProecess extends DataProcess{
 					}
 					values.put("orgi", event.getOrgi()) ;
 					if(values.get("id")==null){
-						values.put("orgi", UKTools.getUUID()) ;
+						values.put("id", UKTools.getUUID()) ;
 					}
 					if(event.getValues()!=null && event.getValues().size() > 0){
 						values.putAll(event.getValues());
