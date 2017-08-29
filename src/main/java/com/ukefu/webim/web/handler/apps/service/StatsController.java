e9a3eb3a6d1c506ad8a293f3f4c7a4fd9a398bf3
优客服-多渠道客服系统 <ricy_fu@126.com>
2017-08-29 16:34:10 +0800

    no commit message

--- src/main/java/com/ukefu/webim/web/handler/apps/service/StatsController.java
+++ src/main/java/com/ukefu/webim/web/handler/apps/service/StatsController.java
@@ -1,8 +1,11 @@
 package com.ukefu.webim.web.handler.apps.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.elasticsearch.common.lang3.StringUtils;
@@ -17,6 +20,7 @@ import com.ukefu.core.UKDataContext;
 import com.ukefu.util.Menu;
 import com.ukefu.util.UKTools;
 import com.ukefu.util.bi.ReportData;
+import com.ukefu.util.bi.UKExcelUtil;
 import com.ukefu.util.bi.model.Level;
 import com.ukefu.webim.service.repository.AgentServiceRepository;
 import com.ukefu.webim.service.repository.CubeService;
@@ -86,6 +90,26 @@ public class StatsController extends Handler{
 		return request(super.createAppsTempletResponse("/apps/service/stats/coment"));
     }
 	
+	@RequestMapping("/stats/coment/exp")
+    @Menu(type = "service" , subtype = "statcoment" , admin= true)
+    public void statcomentexp(ModelMap map , HttpServletRequest request , HttpServletResponse response , @Valid String agent , @Valid String skill , @Valid String begin ,@Valid String end) throws Exception {
+		ReportData reportData = new CubeService("coment.xml", path, dataSource , UKTools.getRequestParam(request)).execute("SELECT [comment].[满意度].members on columns , NonEmptyCrossJoin([time].[日期].members , NonEmptyCrossJoin([skill].[技能组].members,[agent].[坐席].members)) on rows  FROM [满意度]") ;
+		
+		List<SysDic> dicList = UKeFuDic.getInstance().getDic(UKDataContext.UKEFU_SYSTEM_COMMENT_DIC) ;
+		for(Level title : reportData.getCol().getChilderen()){
+			for(SysDic dic : dicList){
+				if(dic.getCode().equals(title.getName())){
+					title.setName(dic.getName());
+				}
+			}
+		}
+		
+		response.setHeader("content-disposition", "attachment;filename=UCKeFu-Report-"+new SimpleDateFormat("yyyy-MM-dd").format(new Date())+".xlsx");
+		new UKExcelUtil(reportData , response.getOutputStream() , "满意度统计").createFile() ;
+		
+		return ;
+    }
+	
 	@RequestMapping("/stats/agent")
     @Menu(type = "service" , subtype = "statagent" , admin= true)
     public ModelAndView statagent(ModelMap map , HttpServletRequest request , @Valid String agent , @Valid String skill , @Valid String begin ,@Valid String end) throws Exception {
@@ -114,5 +138,15 @@ public class StatsController extends Handler{
 		map.addAttribute("agentList", OnlineUserUtils.agents(super.getOrgi(request)))  ;
 		
 		return request(super.createAppsTempletResponse("/apps/service/stats/consult"));
+    }
+	
+	@RequestMapping("/stats/agent/exp")
+    @Menu(type = "service" , subtype = "statagent" , admin= true)
+    public void statagentexp(ModelMap map , HttpServletRequest request , HttpServletResponse response ,@Valid String agent , @Valid String skill , @Valid String begin ,@Valid String end) throws Exception {
+		ReportData reportData = new CubeService("consult.xml", path, dataSource , UKTools.getRequestParam(request)).execute("SELECT {[Measures].[咨询数量],[Measures].[平均等待时长（秒）],[Measures].[平均咨询时长（秒）]} on columns , NonEmptyCrossJoin([time].[日期].members , NonEmptyCrossJoin([skill].[技能组].members,[agent].[坐席].members)) on rows  FROM [咨询]") ;
+		response.setHeader("content-disposition", "attachment;filename=UCKeFu-Report-"+new SimpleDateFormat("yyyy-MM-dd").format(new Date())+".xlsx");
+		new UKExcelUtil(reportData , response.getOutputStream() , "客服坐席统计").createFile() ;
+		
+		return ;
     }
 }
