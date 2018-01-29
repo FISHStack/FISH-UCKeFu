1a376a85e8e1c7a00397eaca259d47160610eb2e
优客服-多渠道客服系统 <ricy_fu@126.com>
2018-01-29 16:54:14 +0800

    1、修正数据库连接池

--- src/main/java/com/ukefu/webim/config/web/DruidConfiguration.java
+++ src/main/java/com/ukefu/webim/config/web/DruidConfiguration.java
@@ -1,126 +1,54 @@
 package com.ukefu.webim.config.web;
 
-import java.sql.SQLException;
-
 import javax.sql.DataSource;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
+@ConditionalOnClass(com.alibaba.druid.pool.DruidDataSource.class)
+@ConditionalOnProperty(name = "spring.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource", matchIfMissing = true)
 public class DruidConfiguration {
 	
-	private Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
-	
-	@Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driverClassName}")
-    private String driverClassName;
-
-    @Value("${spring.datasource.initialSize}")
-    private int initialSize;
-
-    @Value("${spring.datasource.minIdle}")
-    private int minIdle;
-
-    @Value("${spring.datasource.maxActive}")
-    private int maxActive;
-
-    @Value("${spring.datasource.maxWait}")
-    private int maxWait;
-
-    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
-    private int timeBetweenEvictionRunsMillis;
-
-    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
-    private int minEvictableIdleTimeMillis;
-
-    @Value("${spring.datasource.validationQuery}")
-    private String validationQuery;
-
-    @Value("${spring.datasource.testWhileIdle}")
-    private boolean testWhileIdle;
-
-    @Value("${spring.datasource.testOnBorrow}")
-    private boolean testOnBorrow;
-
-    @Value("${spring.datasource.testOnReturn}")
-    private boolean testOnReturn;
-
-    @Value("${spring.datasource.removeAbandoned}")
-    private boolean removeAbandoned;
-
-    @Value("${spring.datasource.removeAbandonedTimeout}")
-    private int removeAbandonedTimeout;
-
-    @Value("${spring.datasource.logAbandoned}")
-    private boolean logAbandoned;
-
-    @Value("${spring.datasource.poolPreparedStatements}")
-    private boolean poolPreparedStatements;
-
-    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
-    private int maxPoolPreparedStatementPerConnectionSize;
-
-    @Value("${spring.datasource.filters}")
-    private String filters;
-
-    @Value("{spring.datasource.connectionProperties}")
-    private String connectionProperties;
-	
-    
-    @Bean     //声明其为Bean实例
-    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
-    public DataSource dataSource(){
-        DruidDataSource datasource = new DruidDataSource();
-
-        datasource.setUrl(this.dbUrl);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        datasource.setDriverClassName(driverClassName);
-
-        //configuration
-        datasource.setInitialSize(initialSize);
-        datasource.setMinIdle(minIdle);
-        datasource.setMaxActive(maxActive);
-        datasource.setMaxWait(maxWait);
-        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        datasource.setValidationQuery(validationQuery);
-        datasource.setTestWhileIdle(testWhileIdle);
-        datasource.setTestOnBorrow(testOnBorrow);
-        datasource.setTestOnReturn(testOnReturn);
-        datasource.setRemoveAbandoned(removeAbandoned);
-        datasource.setRemoveAbandonedTimeout(removeAbandonedTimeout);
-        datasource.setLogAbandoned(logAbandoned);
-        datasource.setPoolPreparedStatements(poolPreparedStatements);
-        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-        try {
-            datasource.setFilters(filters);
-        } catch (SQLException e) {
-            logger.error("druid configuration initialization filter", e);
-        }
-        datasource.setConnectionProperties(connectionProperties);
-
-        return datasource;
+	@SuppressWarnings("unchecked")
+    protected <T> T createDataSource(DataSourceProperties properties,
+                                     Class<? extends DataSource> type) {
+        return (T) properties.initializeDataSourceBuilder().type(type).build();
     }
+
+    /**
+     * @see org.springframework.boot.autoconfigure.jdbc.DataSourceConfiguration.Tomcat 仿写的你可以去了解
+     * @param properties 读入的配置
+     * @return DruidDataSource
+     */
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid")
+    public com.alibaba.druid.pool.DruidDataSource dataSource(DataSourceProperties properties) {
+
+        com.alibaba.druid.pool.DruidDataSource dataSource = createDataSource(
+                properties, com.alibaba.druid.pool.DruidDataSource.class);
+
+        DatabaseDriver databaseDriver = DatabaseDriver.fromJdbcUrl(properties.determineUrl());
+
+        String validationQuery = databaseDriver.getValidationQuery();
+        if (validationQuery != null) {
+            dataSource.setTestOnBorrow(true);
+            dataSource.setValidationQuery(validationQuery);
+        }
+
+        return dataSource;
+    }
+
     
 	/**
 	 * 注册一个StatViewServlet
