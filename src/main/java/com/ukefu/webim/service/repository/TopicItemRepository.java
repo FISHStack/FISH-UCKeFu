cfe452718d81effef7daa577aff791765617706e
优客服-多渠道客服系统 <ricy_fu@126.com>
2018-02-02 00:13:36 +0800

    1、知识库关联表

---
+++ src/main/java/com/ukefu/webim/service/repository/TopicItemRepository.java
@@ -0,0 +1,11 @@
+package com.ukefu.webim.service.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ukefu.webim.web.model.TopicItem;
+
+public interface TopicItemRepository extends JpaRepository<TopicItem, String> {
+	public abstract List<TopicItem> findByTopicid(String topicid);
+}
