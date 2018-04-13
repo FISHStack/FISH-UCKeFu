5adf772bff1dea8cf6ca90cab9e8f28f6083361b
贝密 <open@ukewo.cn>
2018-04-13 18:59:14 +0800

    no commit message

--- src/main/java/com/ukefu/webim/service/repository/CubeService.java
+++ src/main/java/com/ukefu/webim/service/repository/CubeService.java
@@ -46,7 +46,7 @@ public class CubeService {
 		schemaFile = new File(mdxFileDir , UKTools.getUUID()+".xml") ;
 		StringWriter writer = new StringWriter();
 		IOUtils.copy(CubeService.class.getClassLoader().getResourceAsStream(SCHEMA_DATA_PATH+xml), writer, "UTF-8"); 
-		FileUtils.writeByteArrayToFile(schemaFile,UKTools.getTemplet(writer.toString(), requestValues) .getBytes("UTF-8"));	//使用系统默认编码
+		FileUtils.write(schemaFile,UKTools.getTemplet(writer.toString(), requestValues) , "UTF-8");	//使用系统默认编码
 	}
 	
 	public CubeService(String xml , String path , DataSourceService dataSource , Map<String,Object> requestValues,boolean isContentStr) throws IOException, TemplateException {
@@ -57,11 +57,11 @@ public class CubeService {
 		}
 		schemaFile = new File(mdxFileDir , UKTools.getUUID()+".xml") ;
 		if(isContentStr) {
-			FileUtils.writeByteArrayToFile(schemaFile,UKTools.getTemplet(xml, requestValues) .getBytes());	//使用系统默认编码
+			FileUtils.write(schemaFile,UKTools.getTemplet(xml, requestValues) , "UTF-8");	//使用系统默认编码
 		}else {
 			StringWriter writer = new StringWriter();
 			IOUtils.copy(CubeService.class.getClassLoader().getResourceAsStream(SCHEMA_DATA_PATH+xml), writer, "UTF-8"); 
-			FileUtils.writeByteArrayToFile(schemaFile,UKTools.getTemplet(writer.toString(), requestValues) .getBytes("UTF-8"));	//使用系统默认编码
+			FileUtils.write(schemaFile,UKTools.getTemplet(writer.toString(), requestValues) , "UTF-8");	//使用系统默认编码
 		}
 	}
 	
