cffdd4ada967b1e09b4e5350d78fbd17d1dc1b27
贝密 <open@ukewo.cn>
2018-05-16 12:19:30 +0800

    no commit message

--- src/main/java/com/ukefu/webim/util/server/handler/IMEventHandler.java
+++ src/main/java/com/ukefu/webim/util/server/handler/IMEventHandler.java
@@ -162,7 +162,7 @@ public class IMEventHandler
     	 * 以下代码主要用于检查 访客端的字数限制
     	 */
     	CousultInvite invite = OnlineUserUtils.cousult(data.getAppid(),data.getOrgi(), UKDataContext.getContext().getBean(ConsultInviteRepository.class));
-    	if(invite!=null) {
+    	if(invite!=null && invite.getMaxwordsnum() > 0) {
 	    	if(!StringUtils.isBlank(data.getMessage()) && data.getMessage().length() > invite.getMaxwordsnum()){
 	    		data.setMessage(data.getMessage().substring(0 , invite.getMaxwordsnum()));
 	    	}
