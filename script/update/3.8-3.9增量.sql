0797b7e2ebb72875adfe8827a9b11a97f68697f2
贝密 <open@ukewo.cn>
2018-05-21 15:14:07 +0800

    no commit message

---
+++ script/update/3.8-3.9增量.sql
@@ -0,0 +1,31 @@
+ALTER TABLE uk_act_batch ADD execnum int DEFAULT 0 COMMENT "导入次数";
+
+CREATE TABLE `uk_act_formfilter` (
+  `ID` varchar(32) NOT NULL,
+  `NAME` varchar(50) DEFAULT NULL,
+  `CODE` varchar(50) DEFAULT NULL,
+  `CREATETIME` datetime DEFAULT NULL,
+  `CREATER` varchar(32) DEFAULT NULL,
+  `UPDATETIME` datetime DEFAULT NULL,
+  `ORGI` varchar(32) DEFAULT NULL,
+  `USERNAME` varchar(50) DEFAULT NULL,
+  `STATUS` varchar(50) DEFAULT NULL,
+  `PARENTID` varchar(32) DEFAULT NULL COMMENT '上级ID',
+  `FILTERTYPE` varchar(32) DEFAULT NULL COMMENT '筛选类型（批次筛选/元数据筛选）',
+  `BATID` varchar(32) DEFAULT NULL COMMENT '筛选表单使用的批次ID',
+  `TABLEID` varchar(32) DEFAULT NULL COMMENT '筛选表单使用元数据ID',
+  `DATASTATUS` tinyint(4) DEFAULT '0' COMMENT '数据状态',
+  `EXECNUM` INT DEFAULT '0' COMMENT '执行次数',
+  `INX` int(11) DEFAULT '0' COMMENT '分类排序序号',
+  `ORGAN` varchar(32) DEFAULT NULL,
+  `DESCRIPTION` text,
+  `execnum` int(11) DEFAULT '0' COMMENT '导入次数',
+  `filternum` int(11) DEFAULT '0' COMMENT '筛选次数',
+  `conditional` int(11) DEFAULT '0' COMMENT '条件个数',
+
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+ALTER TABLE uk_callcenter_event ADD datestr varchar(32) DEFAULT 0 COMMENT '坐席通话日期（yyyy-MM-dd）用于每小时通话数量折线图';
+ALTER TABLE uk_callcenter_event ADD hourstr varchar(32) DEFAULT 0 COMMENT '坐席通话时间小时（HH）用于每小时通话数量折线图';
