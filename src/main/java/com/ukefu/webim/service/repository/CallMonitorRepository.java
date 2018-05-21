ead303c41626c8a1e87453479564209b617d4828
云维翔 <yunweixiang@ukewo.cn>
2018-05-21 10:45:11 +0800

    no commit message

---
+++ src/main/java/com/ukefu/webim/service/repository/CallMonitorRepository.java
@@ -0,0 +1,34 @@
+package com.ukefu.webim.service.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ukefu.webim.web.model.CallMonitor;
+import com.ukefu.webim.web.model.Organ;
+
+
+/*
+ * 坐席监控表 -- JPA接口
+ */
+public interface CallMonitorRepository extends JpaRepository<CallMonitor, String> {
+	
+	public abstract CallMonitor findByUseridAndOrgi(String userid,String orgi);
+	
+	public abstract CallMonitor findByOrgiAndAgentno(String orgi,String agentno);
+	
+	public abstract List<CallMonitor> findByOrgi(String orgi);
+
+	public abstract List<CallMonitor> findByOrgiAndStatus(String orgi, String status);
+	
+	public abstract List<CallMonitor> findByOrgiAndCode(String orgi, String code);
+	
+	public abstract Long countByOrgiAndStatus(String orgi, String status);
+	
+	public abstract List<CallMonitor> findByOrgiAndOrgan(String orgi ,String organ);
+	
+	
+	
+}
