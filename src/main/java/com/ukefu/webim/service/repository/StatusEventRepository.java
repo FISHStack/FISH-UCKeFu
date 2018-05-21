2cf85d5a791663542dd43e186f6c553056ae0b1b
云维翔 <yunweixiang@ukewo.cn>
2018-05-21 11:07:55 +0800

    no commit message

--- src/main/java/com/ukefu/webim/service/repository/StatusEventRepository.java
+++ src/main/java/com/ukefu/webim/service/repository/StatusEventRepository.java
@@ -1,15 +1,21 @@
 package com.ukefu.webim.service.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ukefu.webim.web.model.StatusEvent;
 
 public interface StatusEventRepository extends JpaRepository<StatusEvent, String> {
 
 	public StatusEvent findById(String id);
+	
+	public StatusEvent findByIdOrBridgeid(String id,String bridgeid);
 	
 	public Page<StatusEvent> findByAni(String ani , Pageable page) ;
 	
@@ -26,4 +32,15 @@ public interface StatusEventRepository extends JpaRepository<StatusEvent, String
 	public Page<StatusEvent> findAll(Specification<StatusEvent> spec, Pageable pageable);  //分页按条件查询 
 
 	public int countByAgent(String agent) ;
+	
+	public int countByAniOrCalled(String ani,String called);
+	
+	public int countByAni(String ani);
+	
+	public int countByCalled(String called);
+	
+	
+	
+	@Query("select HOUR(starttime) as Hour ,count(*) as count  from StatusEvent where orgi = ?1 and DATE_FORMAT(starttime,'%Y-%m-%d') = ?2 group by HOUR(starttime) order by HOUR(starttime)")
+	List<Object> findByOrgiAndStarttime(String orgi , String start);
 }
