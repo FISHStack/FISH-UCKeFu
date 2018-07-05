184d81f9078d6a093aba5d727cb3f5fef99fb444
天翼 <zhangtianyi@ukewo.cn>
2018-07-05 14:38:40 +0800

    no commit message

--- src/main/java/com/ukefu/webim/service/es/ContactsRepository.java
+++ src/main/java/com/ukefu/webim/service/es/ContactsRepository.java
@@ -1,9 +1,12 @@
 package com.ukefu.webim.service.es;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.ukefu.webim.service.es.ContactsEsCommonRepository;
 import com.ukefu.webim.web.model.Contacts;
 
 public interface ContactsRepository extends  ElasticsearchRepository<Contacts, String> , ContactsEsCommonRepository {
+	public int countByPhoneAndOrgi(String phone,String orgi);
 }
