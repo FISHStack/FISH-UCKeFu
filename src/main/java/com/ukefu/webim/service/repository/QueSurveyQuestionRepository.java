2025798dab659d9b4ed8a9a03ae447a8f7026a0c
羽丰乐成 <yunweixiang@ukewo.cn>
2018-07-17 18:38:14 +0800

    no commit message

--- src/main/java/com/ukefu/webim/service/repository/QueSurveyQuestionRepository.java
+++ src/main/java/com/ukefu/webim/service/repository/QueSurveyQuestionRepository.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ukefu.webim.web.model.QueSurveyAnswer;
 import com.ukefu.webim.web.model.QueSurveyProcess;
 import com.ukefu.webim.web.model.QueSurveyQuestion;
 
 public abstract interface QueSurveyQuestionRepository extends JpaRepository<QueSurveyQuestion, String>{
 
-  public abstract Page<QueSurveyQuestion> findByOrgi(String orgi ,Pageable paramPageable);
+  public abstract Page<QueSurveyQuestion> findByProcessidAndOrgi(String processid,String orgi ,Pageable paramPageable);
 
   public abstract List<QueSurveyQuestion> findByOrgi(String orgi);
   
