488b989470377eb7d94ec2e08c9017d307354add
贝密 <open@ukewo.cn>
2018-08-21 14:33:45 +0800

    no commit message

--- src/main/java/com/ukefu/webim/web/handler/api/rest/ApiIMController.java
+++ src/main/java/com/ukefu/webim/web/handler/api/rest/ApiIMController.java
@@ -1,5 +1,9 @@
 package com.ukefu.webim.web.handler.api.rest;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -18,13 +22,16 @@ import com.ukefu.core.UKDataContext;
 import com.ukefu.util.Menu;
 import com.ukefu.util.extra.DataExchangeInterface;
 import com.ukefu.webim.service.acd.ServiceQuene;
+import com.ukefu.webim.service.repository.AgentServiceSatisRepository;
 import com.ukefu.webim.service.repository.ChatMessageRepository;
 import com.ukefu.webim.service.repository.ConsultInviteRepository;
 import com.ukefu.webim.util.OnlineUserUtils;
 import com.ukefu.webim.util.RestResult;
 import com.ukefu.webim.util.RestResultType;
 import com.ukefu.webim.web.handler.Handler;
+import com.ukefu.webim.web.model.AgentServiceSatis;
 import com.ukefu.webim.web.model.AiConfig;
+import com.ukefu.webim.web.model.UKeFuDic;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -40,6 +47,9 @@ public class ApiIMController extends Handler{
 	@Autowired
 	private ChatMessageRepository chatMessageRes ;
 	
+	@Autowired
+	private AgentServiceSatisRepository agentServiceSatisRes ;
+	
 	/**
 	 * 返回在线网站配置
 	 * @param request
@@ -50,7 +60,11 @@ public class ApiIMController extends Handler{
 	@Menu(type = "apps" , subtype = "webim" , access = true)
 	@ApiOperation("获取在线客服")
     public ResponseEntity<RestResult> list(HttpServletRequest request , @PathVariable String id) {
-        return new ResponseEntity<>(new RestResult(RestResultType.OK, OnlineUserUtils.cousult(id ,null, consultInviteRepository)), HttpStatus.OK);
+		Map<String,Object> configMap = new HashMap<String,Object>();
+		configMap.put("invite" , OnlineUserUtils.cousult(id ,null, consultInviteRepository)) ;
+		configMap.put("commentList" , UKeFuDic.getInstance().getDic(UKDataContext.UKEFU_SYSTEM_COMMENT_DIC)) ;
+		configMap.put("commentItemList" , UKeFuDic.getInstance().getDic(UKDataContext.UKEFU_SYSTEM_COMMENT_ITEM_DIC)) ;
+        return new ResponseEntity<>(new RestResult(RestResultType.OK, configMap), HttpStatus.OK);
     }
 	
 	/**
@@ -110,5 +124,47 @@ public class ApiIMController extends Handler{
 	@ApiOperation("获取在线客服会话历史消息")
     public ResponseEntity<RestResult> agent(HttpServletRequest request , @Valid String orgi) {
         return new ResponseEntity<>(new RestResult(RestResultType.OK, ServiceQuene.getAgentReport(orgi)), HttpStatus.OK);
+    }
+	
+	/**
+	 * 返回访客历史消息
+	 * @param request
+	 * @param userid	访客ID
+	 * @p 分页信息
+	 * @return
+	 */
+	@RequestMapping(value = "/statf", method = RequestMethod.GET)
+	@Menu(type = "apps" , subtype = "webim" , access = true)
+	@ApiOperation("获取满意度调查")
+    public ResponseEntity<RestResult> statf(HttpServletRequest request , @Valid String orgi) {
+		Map<String,Object> statfMap = new HashMap<String,Object>();
+		statfMap.put("commentList" , UKeFuDic.getInstance().getDic(UKDataContext.UKEFU_SYSTEM_COMMENT_DIC)) ;
+		statfMap.put("commentItemList" , UKeFuDic.getInstance().getDic(UKDataContext.UKEFU_SYSTEM_COMMENT_ITEM_DIC)) ;
+        return new ResponseEntity<>(new RestResult(RestResultType.OK, statfMap), HttpStatus.OK);
+    }
+	
+	/**
+	 * 返回访客历史消息
+	 * @param request
+	 * @param userid	访客ID
+	 * @p 分页信息
+	 * @return
+	 */
+	@RequestMapping(value = "/satis", method = RequestMethod.POST)
+	@Menu(type = "apps" , subtype = "webim" , access = true)
+	@ApiOperation("获取满意度调查")
+    public ResponseEntity<RestResult> satis(HttpServletRequest request , @Valid String orgi , @Valid AgentServiceSatis satis) {
+		if(satis!=null && !StringUtils.isBlank(satis.getId())){
+    		int count = agentServiceSatisRes.countById(satis.getId()) ;
+    		if(count == 1){
+    			if(!StringUtils.isBlank(satis.getSatiscomment()) && satis.getSatiscomment().length() > 255){
+    				satis.setSatiscomment(satis.getSatiscomment().substring(0 , 255));
+    			}
+    			satis.setSatisfaction(true);
+    			satis.setSatistime(new Date());
+    			agentServiceSatisRes.save(satis) ;
+    		}
+    	}
+        return new ResponseEntity<>(new RestResult(RestResultType.OK), HttpStatus.OK);
     }
 }
